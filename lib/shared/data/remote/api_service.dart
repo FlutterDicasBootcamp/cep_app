d807c7ce17c75278b735ac774f9795234f34b182
alickbuscariolli <alickbuscariolli@gmail.com>
2024-03-12 21:37:10 +1100

    15 - DioService

--- lib/shared/data/remote/api_service.dart
+++ lib/shared/data/remote/api_service.dart
@@ -4,6 +4,6 @@ import 'package:cep_app/shared/data/remote/errors/api_exception.dart';
 import '../models/api_response_model.dart';
 
 abstract interface class ApiService {
-  Future<Either<ApiException, ApiResponseModel>> get(String endPoint,
+  Future<Either<ApiException, ApiResponseModel>> get<T>(String endPoint,
       {Map<String, dynamic>? queryParams});
 }
