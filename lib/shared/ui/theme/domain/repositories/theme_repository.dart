241aa0f7de2c7819e4ae7d2a0b3e73126e01abbd
alickbuscariolli <alickbuscariolli@gmail.com>
2024-06-11 00:25:43 +1000

    20 - Criando os Theme Data Sources

--- lib/shared/ui/theme/domain/repositories/theme_repository.dart
+++ lib/shared/ui/theme/domain/repositories/theme_repository.dart
@@ -3,7 +3,7 @@ import 'package:cep_app/shared/data/async/either.dart';
 import '../../errors/theme_local_exception.dart';
 
 abstract interface class ThemeRepository {
-  Future<bool> getIsLightTheme();
+  Future<Either<ThemeLocalException, bool>> getIsLightTheme();
 
   Future<Either<ThemeLocalException, void>> setIsLightTheme(bool isLightTheme);
 }
