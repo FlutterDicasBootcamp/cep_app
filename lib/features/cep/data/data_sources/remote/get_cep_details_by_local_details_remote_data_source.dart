0165dd7e7e50309331223b8160b1fc4e1fb39b26
alickbuscariolli <alickbuscariolli@gmail.com>
2024-09-10 21:18:07 +1000

    38 - Get Cep Details By Local Details Remote Data Source

---
+++ lib/features/cep/data/data_sources/remote/get_cep_details_by_local_details_remote_data_source.dart
@@ -0,0 +1,40 @@
+import 'package:cep_app/features/cep/data/data_sources/errors/cep_exceptions.dart';
+import 'package:cep_app/features/cep/data/models/cep_response_model.dart';
+import 'package:cep_app/features/cep/domain/entities/get_cep_details_by_local_details_body.dart';
+import 'package:cep_app/shared/data/async/either.dart';
+import 'package:cep_app/shared/data/remote/api_service.dart';
+import 'package:cep_app/shared/data/remote/errors/api_exception.dart';
+import 'package:cep_app/shared/data/remote/errors/no_internet_exception.dart';
+
+abstract interface class GetCepDetailsByLocalDetailsRemoteDataSource {
+  Future<Either<CepRemoteException, List<CepResponseModel>>> call(
+      GetCepDetailsByLocalDetailsBody localDetailsBody);
+}
+
+class GetCepDetailsByLocalDetailsRemoteDataSourceImpl
+    implements GetCepDetailsByLocalDetailsRemoteDataSource {
+  final ApiService _api;
+
+  GetCepDetailsByLocalDetailsRemoteDataSourceImpl(this._api);
+
+  @override
+  Future<Either<CepRemoteException, List<CepResponseModel>>> call(
+      GetCepDetailsByLocalDetailsBody localDetailsBody) async {
+    final cepDetailsByLocalDetailsEither = await _api.get(
+        '${localDetailsBody.estado}/${localDetailsBody.cidade}/${localDetailsBody.rua}/json/');
+
+    switch (cepDetailsByLocalDetailsEither) {
+      case Left(value: final l):
+        return switch (l.errorStatus) {
+          ErrorStatus.noConnection => throw NoInternetException(),
+          ErrorStatus.badRequest =>
+            Left(CepRemoteException(message: l.message!)),
+          _ => Left(CepRemoteException(message: l.message))
+        };
+      case Right(value: final r):
+        return Right((r.data as List)
+            .map((cepResponse) => CepResponseModel.fromMap(cepResponse))
+            .toList());
+    }
+  }
+}
