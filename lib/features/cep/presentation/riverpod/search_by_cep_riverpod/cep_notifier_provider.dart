afc19e9000eb66d20cac0d63b503489397118e12
alickbuscariolli <alickbuscariolli@gmail.com>
2025-07-03 18:35:00 +1000

    50 - Criando tab Search By Cep - Parte 1

--- lib/features/cep/presentation/riverpod/search_by_cep_riverpod/cep_notifier_provider.dart
+++ lib/features/cep/presentation/riverpod/search_by_cep_riverpod/cep_notifier_provider.dart
@@ -1,9 +1,11 @@
 import 'package:cep_app/features/cep/domain/providers/get_cep_details_provider.dart';
 import 'package:cep_app/features/cep/domain/use_cases/get_cep_details_by_cep.dart';
 import 'package:cep_app/features/cep/presentation/riverpod/search_by_cep_riverpod/search_by_cep_notifier.dart';
+import 'package:cep_app/features/cep/presentation/riverpod/search_by_cep_riverpod/search_by_cep_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final searchByCepNotifierProvider = StateNotifierProvider((ref) {
+final searchByCepNotifierProvider =
+    StateNotifierProvider<SearchByCepNotifier, SearchByCepState>((ref) {
   final getCepDetailsByCepInstance =
       ref.read<GetCepDetailsByCep>(getCepDetailsByCep);
 
